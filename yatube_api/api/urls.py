38c80d5739d522535cbfdfb0bccbd19d1abd8814
Александр Денисов <akatsuki_sanya@vk.com>
2024-05-29 16:12:48 +0500

    Made a Post view set whick can send get anonymoust request

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from .views import PostViewSet
+from django.urls import include, path
+from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register(r'v1/posts', PostViewSet)
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,16 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets
+from rest_framework import permissions
+# from rest_framework.pagination import PageNumberPagination
+
+from posts.models import Post
+from .serializers import PostSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    # pagination_class = PageNumberPagination
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
