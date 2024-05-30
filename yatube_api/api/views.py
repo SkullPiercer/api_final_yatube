dce7f6744ba2174ebe2bec360c6a1c92adcd22bf
Александр Денисов <akatsuki_sanya@vk.com>
2024-05-30 12:20:25 +0500

    Add an adding comment funcs in  viewset

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -22,7 +22,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = (AuthorOrReadOnly,)
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
